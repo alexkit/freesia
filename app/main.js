6098e6e2d66a207a5bf8fe7e5c4591a9586c7f9c
llricho <lloyd.richardson7@gmail.com>
2016-08-26 17:10:27 +1000

    Added simple server to main.js

--- app/main.js
+++ app/main.js
@@ -1,3 +1,21 @@
-/**
- * Created by lloydrichardson on 25/08/2016.
- */
+var http = require("http");
+var matcher = require("./matcher");
+var scenarioData = require("./resources/scenario_data.json");
+
+http.createServer(function (request, response) {
+
+    request.on('data', function (chunk) {
+        var result = matcher.findMatch(chunk.toString(), scenarioData);
+        //TODO pass result to the engine module for execution
+        console.log(result.stepID);
+    });
+
+    request.on('end', function () {
+        // empty 200 OK response for now
+        response.writeHead(200, "OK", {'Content-Type': 'text/html'});
+        response.end();
+    });
+
+}).listen(8080);
+
+console.log("Server Running on 8080");
